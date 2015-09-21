05930d2cdb2822b01439012a257ab1e203a4d639
Oscar Evertsson <oscar.evertsson@live.com>
2015-09-21 17:31:00 +0200

    Added handler for github webhook.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var restify = require('restify');
+var exec = require('child_process').exec;
+
 
 function respond(req, res, next) {
 	res.send('hello ' + req.params.name);
@@ -11,6 +13,29 @@ var server = restify.createServer({
 
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
+
+
+server.get('/',function(req,res,next){
+	res.send("hello!");
+	next();
+});
+
+server.post('/hooks/webhook',function(req,res,next){
+	res.status(200);
+	executeGitPull();
+	next();
+});
+
+function executeGitPull(){
+	exec('git pull',
+		function (error, stdout, stderr) {
+			console.log('stdout: ' + stdout);
+			console.log('stderr: ' + stderr);
+			if (error) {
+				console.error('exec error: ' + error);
+			}
+		});
+}
 
 server.listen(8080, function() {
 	console.log('%s listening at %s', server.name, server.url);
