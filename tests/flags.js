04fd20ee76b268686cf6cf5be9d7961c1fed6643
Mats Högberg <mats@hgbrg.se>
2015-09-23 11:25:39 +0200

    Made the descriptions of a few tests more clear

--- tests/flags.js
+++ tests/flags.js
@@ -15,7 +15,7 @@ describe('Flags', function () {
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, flagType must be an integer" when flagType is not an integer', function (done) {
+        it('should respond with status code 400 and the message "Bad request, flagType must be an integer" when flagType is null', function (done) {
             client.post('/flags', { flagType: null }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, flagType must be an integer');
@@ -23,7 +23,7 @@ describe('Flags', function () {
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, flagType must be an integer" when flagType is not an integer', function (done) {
+        it('should respond with status code 400 and the message "Bad request, flagType must be an integer" when flagType is a string', function (done) {
             client.post('/flags', { flagType: 'abc' }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, flagType must be an integer');
@@ -47,50 +47,58 @@ describe('Flags', function () {
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType = 1 and comment is not a string', function (done) {
-            client.post('/flags', { flagType: 1, comment: null }, function (err, req, res, payload) {
+        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType is valid and comment is null', function (done) {
+            client.post('/flags', { flagType: 2, comment: null }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be a string');
                 done();
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType = 1 and comment is not a string', function (done) {
-            client.post('/flags', { flagType: 1, comment: 123 }, function (err, req, res, payload) {
+        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType is valid and comment is an integer', function (done) {
+            client.post('/flags', { flagType: 2, comment: 123 }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be a string');
                 done();
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType = 1 and comment is not a string', function (done) {
-            client.post('/flags', { flagType: 1, comment: true }, function (err, req, res, payload) {
+        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType is valid and comment is a boolean', function (done) {
+            client.post('/flags', { flagType: 2, comment: true }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be a string');
                 done();
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType = 1 and comment is not a string', function (done) {
-            client.post('/flags', { flagType: 1, comment: [] }, function (err, req, res, payload) {
+        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType is valid and comment is an array', function (done) {
+            client.post('/flags', { flagType: 2, comment: [] }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be a string');
                 done();
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType = 1 and comment is not a string', function (done) {
-            client.post('/flags', { flagType: 1, comment: { foo: 'bar' } }, function (err, req, res, payload) {
+        it('should respond with status code 400 and the message "Bad request, comment must be a string" when flagType is valid and comment is an object', function (done) {
+            client.post('/flags', { flagType: 2, comment: { foo: 'bar' } }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be a string');
                 done();
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be more than 5 characters" when flagType = 1 and comment is a string shorter than 5 characters', function (done) {
+        it('should respond with status code 400 and the message "Bad request, comment must be more than 5 characters" when flagType = 1 and comment is an empty string', function (done) {
             client.post('/flags', { flagType: 1, comment: '' }, function (err, req, res, payload) {
                 assert.equal(400, res.statusCode);
                 assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be more than 5 characters');
+                done();
+            });
+        });
+
+        it('should respond with status code 200 when flagType is valid and not 1, and comment is an empty string', function (done) {
+            client.post('/flags', { flagType: 2, comment: '' }, function (err, req, res, payload) {
+                assert.equal(200, res.statusCode);
+                assert.notProperty(payload, 'errorMessage');
                 done();
             });
         });
@@ -103,15 +111,7 @@ describe('Flags', function () {
             });
         });
 
-        it('should respond with status code 400 and the message "Bad request, comment must be more than 5 characters" when flagType = 1 and comment is a string shorter than 5 characters', function (done) {
-            client.post('/flags', { flagType: 1, comment: 'abcd' }, function (err, req, res, payload) {
-                assert.equal(400, res.statusCode);
-                assert.propertyVal(payload, 'errorMessage', 'Bad request, comment must be more than 5 characters');
-                done();
-            });
-        });
-
-        it('should respond with status code 200 when flagType exists', function (done) {
+        it('should respond with status code 200 when flagType exists and is not 1, and comment is absent', function (done) {
             client.post('/flags', { flagType: 2 }, function (err, req, res, payload) {
                 assert.equal(200, res.statusCode);
                 assert.notProperty(payload, 'errorMessage');
