2b3cfcabed845fdb7e3563ae78c8f94d9b40ffba
hjorthjort <rikard.hjort@gmail.com>
2015-09-27 01:44:58 +0200

    Indentation fixes

--- db/init_db.sql
+++ db/init_db.sql
@@ -1,6 +1,7 @@
 CREATE TABLE flag_descriptions (
-_id SERIAL PRIMARY KEY,
-name VARCHAR(80)) ;
+    _id SERIAL PRIMARY KEY,
+    name VARCHAR(80)
+);
 
 CREATE TABLE reports (
     _id SERIAL PRIMARY KEY,
@@ -8,5 +9,4 @@ CREATE TABLE reports (
     time_reported TIMESTAMP WITH TIME ZONE NOT NULL,
     comment TEXT,
     active BOOLEAN DEFAULT TRUE -- Gives ability to "delete" a flag without removing it from history
-)
-
+);
